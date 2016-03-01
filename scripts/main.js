b42f3f6af47c58708afe42fbd123caf88e919ced
Oleksandra Tsalko <oleks.tsalko@gmail.com>
2016-03-01 11:05:44 +0100

    remove half-done things

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
 (function() {
-    // load the code snippets
-    // (c) initial idea is owned by Lea Verou (btw, she is a girl!)
-
-    $('[data-src]').each(function(index) {
-        var src = $( this ).attr('data-src');
-        $( this ).load(src);
-    });
+    // put your js code here
 }());
